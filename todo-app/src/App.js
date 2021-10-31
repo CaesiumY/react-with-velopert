8776ac273e6470da973cff5c11ecd230155265a2
Changsik Yoon <dbs2636@gmail.com>
2021-10-31 20:14:51 +0900

    feat: Add TodoTemplate

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import TodoTemplate from './components/TodoTemplate/TodoTemplate';
 
 const App = () => {
-  return <div>hello world</div>;
+  return <TodoTemplate>Todo App을 만들자</TodoTemplate>;
 };
 
 export default App;
