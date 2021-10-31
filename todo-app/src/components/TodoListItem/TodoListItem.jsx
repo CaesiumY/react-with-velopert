bb012fb4f1149b2dd0b62b1dcfe5abee845a82c9
Changsik Yoon <dbs2636@gmail.com>
2021-10-31 21:23:12 +0900

    feat: Add initial todo state

--- todo-app/src/components/TodoListItem/TodoListItem.jsx
+++ todo-app/src/components/TodoListItem/TodoListItem.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
 import './TodoListItem.scss';
-import { MdCheckBoxOutlineBlank, MdRemoveCircleOutline } from 'react-icons/md';
+import {
+  MdCheckBox,
+  MdCheckBoxOutlineBlank,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할 일</div>
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
         <MdRemoveCircleOutline />
