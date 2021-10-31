b155241015db684419f308ab14bfef95ccab3f4f
Changsik Yoon <dbs2636@gmail.com>
2021-10-31 20:45:33 +0900

    feat: Add TodoList

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoInsert from './components/TodoInsert/TodoInsert';
+import TodoList from './components/TodoList/TodoList';
 import TodoTemplate from './components/TodoTemplate/TodoTemplate';
 
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
