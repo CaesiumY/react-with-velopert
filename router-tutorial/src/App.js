819b8bbd6cda8e36a09c3ded5d1159e16055267d
Changsik Yoon <dbs2636@gmail.com>
2021-11-03 01:27:42 +0900

    feat: Add Link

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
     </div>
