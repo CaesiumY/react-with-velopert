557f84ae45c4eea15d1b62fa45cdbdb7d3a07a33
Changsik Yoon <dbs2636@gmail.com>
2021-11-03 15:38:30 +0900

    feat: Add url parameter

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, Route } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
@@ -13,10 +14,17 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
-      <Route path="/about" component={About} />
+      <Route path={["/about", "/info"]} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };
