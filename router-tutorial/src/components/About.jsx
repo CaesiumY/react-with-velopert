cac6301b285f6bd11876cb9cb5f39be6754ea5f9
Changsik Yoon <dbs2636@gmail.com>
2021-11-03 15:56:43 +0900

    feat: Add handle query by URLSearchParams API

--- router-tutorial/src/components/About.jsx
+++ router-tutorial/src/components/About.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-const About = () => {
+const About = ({ location }) => {
+  const query = new URLSearchParams(location.search);
+  const showDetail = query.get("detail") === "true";
+
   return (
     <div>
       <h1>소개</h1>
       <p>리액트 라우터 프로젝트</p>
+
+      {showDetail && <p>detail 쿼리가 true입니다</p>}
     </div>
   );
 };
