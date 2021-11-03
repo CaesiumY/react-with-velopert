1c2a1191ae94d0890f8e9e314cc783a2c8d468fa
Changsik Yoon <dbs2636@gmail.com>
2021-11-03 16:50:24 +0900

    feat: Add Switch and NavLink

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 import About from "./components/About";
 import HistorySample from "./components/HistorySample";
 import Home from "./components/Home";
@@ -23,10 +23,20 @@ const App = () => {
         </li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact />
-      <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path={["/about", "/info"]} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>존재하지 않는 페이지</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
