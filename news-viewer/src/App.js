ad7a1e3933b142bf732225a44ead2374b7736cd2
Changsik Yoon <dbs2636@gmail.com>
2021-11-04 19:46:13 +0900

    feat: Add NewsList and NewsItem components

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,34 +1,8 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import NewsList from "./components/NewsList";
 
 const App = () => {
-  const [data, setData] = useState(null);
-
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${process.env.REACT_APP_API_KEY}`
-      );
-      setData(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows="7"
-          readOnly={true}
-          value={JSON.stringify(data)}
-        ></textarea>
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
