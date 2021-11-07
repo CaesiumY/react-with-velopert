340ae29e55f40a48ce9ccdf6dd8e67d87a13dc22
Changsik Yoon <dbs2636@gmail.com>
2021-11-07 22:52:52 +0900

    feat: Add redux-actions

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,3 +1,5 @@
+import { createAction, handleActions } from "redux-actions";
+
 const CHANGE_INPUT = "todos/CHANGE_INPUT";
 const INSERT = "todos/INSERT";
 const TOGGLE = "todos/TOGGLE";
@@ -5,26 +7,16 @@ const REMOVE = "todos/REMOVE";
 
 let currentId = 3;
 
-export const changeInput = (input) => ({
-  type: CHANGE_INPUT,
-  input,
-});
-export const insert = (text) => ({
-  type: INSERT,
-  todos: {
-    id: currentId++,
-    text,
-    done: false,
-  },
-});
-export const toggle = (id) => ({
-  type: TOGGLE,
-  id,
-});
-export const remove = (id) => ({
-  type: REMOVE,
-  id,
-});
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
+
+export const insert = createAction(INSERT, (text) => ({
+  id: currentId++,
+  text,
+  done: false,
+}));
+
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
 
 const initialState = {
   input: "",
@@ -42,35 +34,25 @@ const initialState = {
   ],
 };
 
-const todos = (state = initialState, action) => {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input,
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos: [...state.todos, action.todos],
-      };
-    case TOGGLE:
-      console.log(state.todos, action);
-      return {
-        ...state,
-        todos: state.todos.map((todo) =>
-          todo.id === action.id ? { ...todo, done: !todo.done } : todo
-        ),
-      };
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.id),
-      };
-
-    default:
-      return state;
-  }
-};
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: [...state.todos, todo],
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+  },
+  initialState
+);
 
 export default todos;
