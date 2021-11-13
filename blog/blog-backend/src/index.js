5d10394a799e8f2480847dc601093e224f1f9b0a
Changsik Yoon <dbs2636@gmail.com>
2021-11-13 22:44:20 +0900

    feat: Add mongoDB using mLab

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const api = require('./api');
 
@@ -13,6 +26,8 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000', 'http://localhost:4000/');
+const port = PORT || 4000;
+
+app.listen(port, () => {
+  console.log('Listening to', `http://localhost:4000/${port}`);
 });
