37d930f430acb38ff18e32a08e113c63788ed005
Changsik Yoon <dbs2636@gmail.com>
2021-11-14 22:25:43 +0900

    feat: Add register and login

--- blog/blog-backend/src/models/user.js
+++ blog/blog-backend/src/models/user.js
@@ -9,13 +9,19 @@ const UserSchema = new Schema({
 });
 
 UserSchema.methods.setPassword = async function (password) {
-  const hash = await bcrypt(password, 10);
+  const hash = await bcrypt.hash(password, 10);
   this.hashedPassword = hash;
 };
 
 UserSchema.methods.checkPassword = async function (password) {
   const result = await bcrypt.compare(password, this.hashedPassword);
   return result;
+};
+
+UserSchema.methods.serialize = function () {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
 };
 
 UserSchema.statics.findByUsername = function (username) {
