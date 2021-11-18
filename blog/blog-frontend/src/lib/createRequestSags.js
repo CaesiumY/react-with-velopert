2590f6795af82ac70a330beb03ea36ac16ae75c3
Changsik Yoon <dbs2636@gmail.com>
2021-11-18 19:20:21 +0900

    feat: Add register and login saga

--- blog/blog-frontend/src/lib/createRequestSags.js
+++ blog/blog-frontend/src/lib/createRequestSags.js
@@ -1,6 +1,13 @@
 import { put, call } from 'redux-saga/effects';
 import { finishLoading, startLoading } from '../modules/loading';
 
+export const createRequestActionSaga = (type) => {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return [type, SUCCESS, FAILURE];
+};
+
 export default function createRequestSaga(type, request) {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
