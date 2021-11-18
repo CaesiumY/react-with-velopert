fabcee53343fc13770897fc879dbfab7601c6b1e
Changsik Yoon <dbs2636@gmail.com>
2021-11-18 19:00:29 +0900

    feat: Add redux-saga

---
+++ blog/blog-frontend/src/lib/createRequestSags.js
@@ -0,0 +1,27 @@
+import { put, call } from 'redux-saga/effects';
+import { finishLoading, startLoading } from '../modules/loading';
+
+export default function createRequestSaga(type, request) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return function* (action) {
+    yield put(startLoading(type));
+
+    try {
+      const response = yield call(request, action.payload);
+      yield put({
+        type: SUCCESS,
+        payload: response.data,
+      });
+    } catch (error) {
+      yield put({
+        type: FAILURE,
+        payload: error,
+        error: true,
+      });
+    } finally {
+      yield put(finishLoading(type));
+    }
+  };
+}
