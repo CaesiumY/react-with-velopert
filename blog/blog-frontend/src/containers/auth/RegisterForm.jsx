c89a4b205419d4542bcb64233a091c601e8ed66a
Changsik Yoon <dbs2636@gmail.com>
2021-11-19 22:49:41 +0900

    feat: Add register

--- blog/blog-frontend/src/containers/auth/RegisterForm.jsx
+++ blog/blog-frontend/src/containers/auth/RegisterForm.jsx
@@ -1,17 +1,54 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import AuthForm from '../../components/auth/AuthForm';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, register } from '../../modules/auth';
+import { check } from '../../modules/user';
 
 const KEY = 'register';
 
 const RegisterForm = () => {
-  const { form } = useSelector(({ auth }) => ({ form: auth.register }));
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+    form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
+  }));
+
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(initializeForm(KEY));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+
+    if (auth) {
+      console.log('회원 가입 성공');
+      console.log(auth);
+      dispatch(check());
+      return;
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      console.log('check API 성공');
+      console.log(user);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [user, navigate]);
 
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -27,7 +64,13 @@ const RegisterForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    // TODO
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      // TODO
+      return;
+    }
+
+    dispatch(register({ username, password }));
   };
 
   return (
