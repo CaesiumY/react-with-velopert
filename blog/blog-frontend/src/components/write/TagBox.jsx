baa29bbfb14fcf4c815e2d644f3d6d92817617d9
Changsik Yoon <dbs2636@gmail.com>
2021-11-22 22:43:52 +0900

    feat: Connect redux store with WritePage contents

--- blog/blog-frontend/src/components/write/TagBox.jsx
+++ blog/blog-frontend/src/components/write/TagBox.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
@@ -76,7 +76,7 @@ const TagList = React.memo(({ tags, onRemove }) => (
   </TagListBlock>
 ));
 
-const TagBox = () => {
+const TagBox = ({ tags, onChangeTags }) => {
   const [input, setInput] = useState('');
   const [localTags, setLocalTags] = useState([]);
 
@@ -85,16 +85,20 @@ const TagBox = () => {
       if (!tag) return;
       if (localTags.includes(tag)) return;
 
-      setLocalTags((prev) => [...prev, tag]);
+      const nextTags = [...localTags, tag];
+      setLocalTags(nextTags);
+      onChangeTags(nextTags);
     },
-    [localTags],
+    [localTags, onChangeTags],
   );
 
   const onRemove = useCallback(
     (tag) => {
-      setLocalTags(localTags.filter((t) => t !== tag));
+      const nextTags = localTags.filter((t) => t !== tag);
+      setLocalTags(nextTags);
+      onChangeTags(nextTags);
     },
-    [localTags],
+    [localTags, onChangeTags],
   );
 
   const onChange = useCallback((e) => {
@@ -109,6 +113,10 @@ const TagBox = () => {
     },
     [input, insertTag],
   );
+
+  useEffect(() => {
+    setLocalTags(tags);
+  }, [tags]);
 
   return (
     <TagBoxBlock>
--- blog/blog-frontend/src/modules/index.js
+++ blog/blog-frontend/src/modules/index.js
@@ -1,13 +1,15 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
-import auth, { authSaga } from './auth';
 import loading from './loading';
+import auth, { authSaga } from './auth';
 import user, { userSaga } from './user';
+import write from './write';
 
 const rootReducer = combineReducers({
   auth,
   loading,
   user,
+  write,
 });
 
 export function* rootSaga() {
