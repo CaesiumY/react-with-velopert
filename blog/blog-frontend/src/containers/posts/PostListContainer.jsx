8f5ab61c99500748f5db1d5a1df48e062d425922
Changsik Yoon <dbs2636@gmail.com>
2021-11-23 23:12:14 +0900

    fix: Update username using useParams instead of useLocation

--- blog/blog-frontend/src/containers/posts/PostListContainer.jsx
+++ blog/blog-frontend/src/containers/posts/PostListContainer.jsx
@@ -1,12 +1,13 @@
 import QueryString from 'qs';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import PostList from '../../components/post/PostList';
 import { listPost } from '../../modules/posts';
 
 const PostListContainer = () => {
   const location = useLocation();
+  const { username } = useParams();
   const dispatch = useDispatch();
   const { posts, error, loading, user } = useSelector(
     ({ posts, loading, user }) => ({
@@ -18,12 +19,12 @@ const PostListContainer = () => {
   );
 
   useEffect(() => {
-    const { tag, username, page } = QueryString.parse(location.search, {
+    const { tag, page } = QueryString.parse(location.search, {
       ignoreQueryPrefix: true,
     });
 
     dispatch(listPost({ tag, username, page }));
-  }, [dispatch, location.search]);
+  }, [dispatch, location.search, username]);
 
   return (
     <PostList
